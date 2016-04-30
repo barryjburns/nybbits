be705a08f97d59451abf0c1586cd3bb7e385b940
Barry J. Burns <barry@barryjburns.com>
2016-04-30 13:29:26 -0500

    stuff

--- nybbits/settings.py
+++ nybbits/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'nybbits.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nybbits',
+	'USER': 'www-data',
+	'PASSWORD': '',
+	'HOST': '',
+	'PORT': ''
     }
 }
 
@@ -101,3 +105,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
